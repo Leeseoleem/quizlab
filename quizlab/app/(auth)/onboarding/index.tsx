8b3dc11cbe1052011089476525beafca0e1363d8
Leeseoleem <leeseorim0029@gmail.com>
2025-06-06 05:51:36 +0900

    test: 컴포넌트 동작 확인용 코드 작성

--- quizlab/app/(auth)/onboarding/index.tsx
+++ quizlab/app/(auth)/onboarding/index.tsx
@@ -1,14 +1,20 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 import { CommonButton } from "@/components/ui/common/buttons/CommonButton";
+import { ModalContainer } from "@/components/ui/Modal/ModalContiner";
 
 export default function OnboardingScreen() {
+  const [openModal, setOpenModal] = useState<boolean>(false);
   return (
-    <SafeAreaView className="flex-1 " edges={["top"]}>
-      <View>
+    <SafeAreaView className="flex-1">
+      <ModalContainer visible={openModal} onClose={() => setOpenModal(false)}>
+        <Text>하이</Text>
+      </ModalContainer>
+      <View className="flex-1 px-4 justify-between">
         <Text>온보딩</Text>
-        <CommonButton label="하이" type="secondary" onPress={() => {}} />
+        <CommonButton label="모달 열기" onPress={() => setOpenModal(true)} />
       </View>
     </SafeAreaView>
   );

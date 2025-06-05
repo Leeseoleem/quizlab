8a954f59de53b0d1e762605f50448009a6cc2fc5
Leeseoleem <leeseorim0029@gmail.com>
2025-06-06 03:22:52 +0900

    feat: common button 컴포넌트 생성

--- quizlab/app/(auth)/onboarding/index.tsx
+++ quizlab/app/(auth)/onboarding/index.tsx
@@ -1,11 +1,14 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, TouchableOpacity } from "react-native";
 
+import { CommonButton } from "@/components/ui/common/buttons/CommonButton";
+
 export default function OnboardingScreen() {
   return (
     <SafeAreaView className="flex-1 " edges={["top"]}>
       <View>
         <Text>온보딩</Text>
+        <CommonButton label="하이" type="secondary" onPress={() => {}} />
       </View>
     </SafeAreaView>
   );

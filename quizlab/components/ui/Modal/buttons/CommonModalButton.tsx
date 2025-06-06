3d81c171618f8cc12ff1d40df76ce856b3017401
Leeseoleem <leeseorim0029@gmail.com>
2025-06-06 21:54:45 +0900

    feat: CommonModalButton 컴포넌트 생성

---
+++ quizlab/components/ui/Modal/buttons/CommonModalButton.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import { TouchableOpacity } from "react-native";
+import { BodyBtn } from "../../Typography";
+
+type ButtonType = "primary" | "secondary" | "disable";
+
+type CommonModalButtonProps = {
+  label: string;
+  type?: ButtonType;
+  onPress: () => void;
+};
+
+export const CommonModalButton = ({
+  label,
+  type = "primary",
+  onPress,
+}: CommonModalButtonProps) => {
+  // 버튼 스타일
+  const buttonClass = clsx(
+    "w-24 h-12 justify-center items-center rounded-xl",
+    type === "secondary" ? "bg-white" : "bg-primary",
+    type === "secondary" && "border border-gray20",
+    type === "disable" && "opacity-60"
+  );
+
+  // 텍스트 스타일
+  const textClass = clsx(type === "secondary" ? "text-black" : "text-white");
+  return (
+    <TouchableOpacity
+      activeOpacity={type === "disable" ? 0.6 : 0.8}
+      onPress={onPress}
+      className={buttonClass}
+      disabled={type === "disable"}
+    >
+      <BodyBtn color={textClass}>{label}</BodyBtn>
+    </TouchableOpacity>
+  );
+};

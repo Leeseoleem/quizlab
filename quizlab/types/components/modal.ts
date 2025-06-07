0a7d52354169c8e6f76ac3bc0993a324296129f7
Leeseoleem <leeseorim0029@gmail.com>
2025-06-08 00:14:11 +0900

    refactor: 각 타입 별 text input props를 export

--- quizlab/types/components/modal.ts
+++ quizlab/types/components/modal.ts
@@ -4,17 +4,19 @@ interface BaseInputProps {
   text: string;
   onChangeText: (text: string) => void;
   maxLength?: number;
-  label: string;
 }
 
 // default 타입
-interface DefaultInputProps extends BaseInputProps {
+export interface DefaultInputProps extends BaseInputProps {
   type: "default";
   onPressClear: () => void;
 }
 
 // large 타입
-interface LargeInputProps extends BaseInputProps {
+export interface LargeInputProps extends BaseInputProps {
   type: "large";
-  // onPressClear 없음!
 }
+
+export type ModalTextBoxProps =
+  | (DefaultInputProps & { label: string })
+  | (LargeInputProps & { label: string });

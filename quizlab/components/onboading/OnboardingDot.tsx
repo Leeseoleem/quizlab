517025c8b20bd1143e2b57903e27336d25fdfda0
Leeseoleem <leeseorim0029@gmail.com>
2025-06-09 17:04:29 +0900

    chore: 온보딩 constants 모듈 통합 import를 위한 index.ts 추가

--- quizlab/components/onboading/OnboardingDot.tsx
+++ quizlab/components/onboading/OnboardingDot.tsx
@@ -3,7 +3,7 @@ import { View } from "react-native";
 
 export const OnboardingDot = ({ selected }: { selected: boolean }) => {
   const dotClass = clsx(
-    "w-2 h-2 mx-1 border-r-4",
+    "w-2 h-2 mx-1 rounded",
     selected ? "bg-primary" : "bg-gray20"
   );
   return <View className={dotClass} />;

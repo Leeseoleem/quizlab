41cf0e09db669b844b5dff6d57f77e3ccc6db329
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-06-09 17:25:05 +0900

    [FEAT] onboarding screen 1차 구현 완료 (#5)
    * chore: 온보딩 용 일러스트 추가
    
    * chore: react-native-onboarding-swiper 라이브러리 설치 및 타입 추가
    
    * chore: react-native-onboarding-swiper 타입 설치
    
    * feat: 온보딩 이미지용 재사용 태그 생성
    
    * chore: image path 파일 생성 및 온보딩 이미지 경로 추가
    
    * chore: 온보딩 이미지 파일 폴더명 수정
    
    * chore: 폴더명 변경에 따른 path 수정
    
    * chore: 온보딩 문구 정리
    
    * chore: 온보딩 버튼 문구 추가
    
    * feat: 온보딩 인디케이터 전용 Dot 컴포넌트 구현
    
    * chore: 온보딩 constants 모듈 통합 import를 위한 index.ts 추가
    
    * chore: 온보딘 전용 title 스타일 추가
    
    * feat: 온보딩 화면 1차 구현 완료
    
    - 추후 로그인 페이지 제작 완료 시 AsyncStorage 온보딩 확인 로직 추가

---
+++ quizlab/constants/onboarding/styles.ts
@@ -0,0 +1,18 @@
+import { GrayColors } from "../Colors";
+
+const onboardingTitleStyle = {
+  fontSize: 24,
+  letterSpacing: -1.6,
+  fontFamily: "Gmarket-Medium",
+  color: GrayColors.black,
+};
+
+const onboardingSubtitleStyle = {
+  fontSize: 14,
+  letterSpacing: -0.8,
+  lineHeight: 20,
+  fontFamily: "Gmarket-Light",
+  color: GrayColors.black,
+};
+
+export { onboardingTitleStyle, onboardingSubtitleStyle };

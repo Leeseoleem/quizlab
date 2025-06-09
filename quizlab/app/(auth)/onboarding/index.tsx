cc7ef7eadb05744f94a2ff44d29812882fc7f440
Leeseoleem <leeseorim0029@gmail.com>
2025-06-09 17:07:50 +0900

    feat: 온보딩 화면 1차 구현 완료
    - 추후 로그인 페이지 제작 완료 시 AsyncStorage 온보딩 확인 로직 추가

--- quizlab/app/(auth)/onboarding/index.tsx
+++ quizlab/app/(auth)/onboarding/index.tsx
@@ -1,11 +1,61 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
+import { router } from "expo-router";
+
+import { GrayColors } from "@/constants/Colors";
+import {
+  OnboardingImages,
+  OnboardingStrings,
+  onboardingTitleStyle,
+  onboardingSubtitleStyle,
+} from "@/constants/onboarding";
+
+import { OnboardingImage } from "@/components/onboading/OnboardingImage";
+import { OnboardingDot } from "@/components/onboading/OnboardingDot";
+import { RoundButton } from "@/components/ui/common/buttons/RoundButton";
 
 export default function OnboardingScreen() {
   return (
     <SafeAreaView className="flex-1">
-      <Text>하이</Text>
+      <Onboarding
+        titleStyles={onboardingTitleStyle}
+        subTitleStyles={onboardingSubtitleStyle}
+        controlStatusBar={false}
+        showSkip={false}
+        showNext={false}
+        showDone={false}
+        bottomBarHighlight={false}
+        DotComponent={({ selected }: { selected: boolean }) => (
+          <OnboardingDot selected={selected} />
+        )}
+        pages={[
+          {
+            backgroundColor: GrayColors.white,
+            image: <OnboardingImage source={OnboardingImages.first} />,
+            title: OnboardingStrings.first.title,
+            subtitle: OnboardingStrings.first.subtitle,
+          },
+          {
+            backgroundColor: GrayColors.white,
+            image: <OnboardingImage source={OnboardingImages.second} />,
+            title: OnboardingStrings.second.title,
+            subtitle: OnboardingStrings.second.subtitle,
+          },
+          {
+            backgroundColor: GrayColors.white,
+            image: <OnboardingImage source={OnboardingImages.third} />,
+            title: OnboardingStrings.third.title,
+            subtitle: OnboardingStrings.third.subtitle,
+          },
+        ]}
+      />
+      <View className="w-full px-4 py-4">
+        <RoundButton
+          label={OnboardingStrings.button}
+          onPress={() => router.replace("/(auth)/login")}
+        />
+      </View>
     </SafeAreaView>
   );
 }

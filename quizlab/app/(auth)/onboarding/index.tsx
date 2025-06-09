04949c2ab7f735673a7953786bd094d052751c59
Leeseoleem <leeseorim0029@gmail.com>
2025-06-09 12:30:34 +0900

    chore: Bottom modal 용 Barrel pattern 사용

--- quizlab/app/(auth)/onboarding/index.tsx
+++ quizlab/app/(auth)/onboarding/index.tsx
@@ -2,19 +2,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
-
-
 export default function OnboardingScreen() {
-
-
-
   return (
     <SafeAreaView className="flex-1">
-     
-
-        <Text>하이</Text>
-
-
+      <Text>하이</Text>
     </SafeAreaView>
   );
 }
---
+++ quizlab/components/ui/bottom-modal/index.ts
@@ -0,0 +1,4 @@
+export { BottomModalContainer } from "./BottomModalContainer";
+export { BottomModalList } from "./BottomModalList";
+export { BottomModalButton } from "./BottomModalButton";
+export { BottomModalHeader } from "./BottomModalHeader";

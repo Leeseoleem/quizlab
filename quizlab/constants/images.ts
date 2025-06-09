b806db21387e85cbb60467f7388b5dfb2e1aa933
Leeseoleem <leeseorim0029@gmail.com>
2025-06-09 16:35:45 +0900

    chore: image path 파일 생성 및 온보딩 이미지 경로 추가

---
+++ quizlab/constants/images.ts
@@ -0,0 +1,5 @@
+export const OnboardingImages = {
+  first: require("@/assets/images/onboarding/first.png"),
+  second: require("@/assets/images/onboarding/second.png"),
+  third: require("@/assets/images/onboarding/third.png"),
+};

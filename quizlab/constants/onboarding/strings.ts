8cf76fa6c9eedb50a97814ae7d2b50c52af4cac0
Leeseoleem <leeseorim0029@gmail.com>
2025-06-09 16:49:27 +0900

    chore: 온보딩 문구 정리

---
+++ quizlab/constants/onboarding/strings.ts
@@ -0,0 +1,17 @@
+export const OnboardingStrings = {
+  first: {
+    title: "나만의 문제집 만들기",
+    subtitle:
+      "폴더도, 문제도 전부 내 맘대로!\n나만의 방식으로 문제집을 만들어보세요!",
+  },
+  second: {
+    title: "다양한 문제 풀이 모드",
+    subtitle:
+      "시간 제한부터 자유롭게 푸는 방식까지,\n내 스타일에 맞춰 문제를 풀어보세요!",
+  },
+  third: {
+    title: "학습 현황 한 눈에 보기",
+    subtitle:
+      "얼마나 풀었고 얼마나 맞췄는지,\n공부한 기록이 전부 차곡차곡 모여 있어요.",
+  },
+};

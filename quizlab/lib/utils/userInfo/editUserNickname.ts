5e87a95ef06592357cd6052d6fdc6782ff96de93
Leeseoleem <leeseorim0029@gmail.com>
2025-06-11 02:04:49 +0900

    feat: 유저 닉네임 업데이트 함수 추가

---
+++ quizlab/lib/utils/userInfo/editUserNickname.ts
@@ -0,0 +1,23 @@
+import { updateProfile } from "firebase/auth";
+import { auth } from "@/lib/firebase/config";
+import { fetchCurrentUserInfo } from "../fetchCurrentUserInfo";
+
+export const editUserNickname = async (newNickname: string) => {
+  const user = auth.currentUser;
+
+  if (user) {
+    try {
+      await updateProfile(user, {
+        displayName: newNickname,
+      });
+      console.log("✅ 닉네임 변경 성공");
+
+      const updatedUser = await fetchCurrentUserInfo();
+      return updatedUser?.nickname;
+    } catch (error) {
+      console.error("닉네임 변경 실패:", error);
+    }
+  } else {
+    console.log("로그인된 유저가 없습니다.");
+  }
+};

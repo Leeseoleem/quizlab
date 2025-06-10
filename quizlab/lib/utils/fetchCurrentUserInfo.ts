60ff5229b081a206568b1ba91decdcd43d7eaa29
Leeseoleem <leeseorim0029@gmail.com>
2025-06-11 02:04:23 +0900

    feat: 현재 로그인한 유저 확인 함수 추가

---
+++ quizlab/lib/utils/fetchCurrentUserInfo.ts
@@ -0,0 +1,22 @@
+import { auth } from "../firebase/config";
+
+export const fetchCurrentUserUid = async () => {
+  const user = auth.currentUser;
+  return user ? user.uid : null;
+};
+
+export const fetchCurrentUserInfo = async () => {
+  const user = auth.currentUser;
+
+  if (user) {
+    return {
+      uid: user.uid,
+      email: user.email,
+      nickname: user.displayName,
+      photoURL: user.photoURL,
+    };
+  } else {
+    // 아직 로그인 정보가 준비 안 된 상태일 수도 있음
+    return null;
+  }
+};

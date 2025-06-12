ddb2ce5bfd957b2d154ddad63beaba63336e5a0f
Leeseoleem <leeseorim0029@gmail.com>
2025-06-12 15:19:58 +0900

    chore: my tab 문자열 상수화

---
+++ quizlab/constants/my/strings.ts
@@ -0,0 +1,21 @@
+export const MyStrings = {
+  header: {
+    title: "내 정보",
+  },
+  editModal: {
+    header: "닉네임 수정하기",
+    label: "닉네임",
+    placeholder: "한글, 영문, 숫자 2~12자",
+  },
+  toast: {
+    nicknameUpdated: "닉네임이 수정되었습니다",
+    nicknameUpdateFailed: "닉네임 수정에 실패하였습니다",
+  },
+  section: {
+    title: "계정 관리",
+    list: {
+      logout: "로그아웃",
+      deleteAccout: "탈퇴하기",
+    },
+  },
+};

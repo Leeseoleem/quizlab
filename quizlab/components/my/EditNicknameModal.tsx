3ee91117a4c51b6f21fe68151c3da9b7e8aeb9f1
Leeseoleem <leeseorim0029@gmail.com>
2025-06-12 15:18:40 +0900

    chore: 닉네임 수정 모달 컴포넌트 strings 적용 및 타입 위치 수정

--- quizlab/components/my/EditNicknameModal.tsx
+++ quizlab/components/my/EditNicknameModal.tsx
@@ -7,42 +7,47 @@ import {
 } from "../ui/Modal";
 
 import { MyStrings } from "@/constants/my/strings";
+import { NICKNAME_VALIDATION } from "@/constants/auth/validationStrings";
 
 interface EditNicknameModalProps {
   isEditModalVisible: boolean;
-  closeEditModal: () => void;
   nickname: string;
   onChangeText: (text: string) => void;
   onPressClear: () => void;
+  disable: boolean;
+  closeEditModal: () => void;
   handleUpdateNickname: () => void;
 }
 
 export const EditNicknameModal = ({
   isEditModalVisible,
-  closeEditModal,
   nickname,
   onChangeText,
   onPressClear,
+  disable,
+  closeEditModal,
   handleUpdateNickname,
 }: EditNicknameModalProps) => {
   return (
     <ModalContainer visible={isEditModalVisible} onClose={closeEditModal}>
       <View className="w-full gap-8">
         <CloseModalHeader
-          label={MyStrings.editLabel}
+          label={MyStrings.editModal.label}
           onPress={closeEditModal}
         />
         <View className="w-full px-6">
           <ModalTextBox
             type="default"
-            label={MyStrings.editLabel}
-            placeholder={MyStrings.editLabel}
+            label={MyStrings.editModal.label}
+            placeholder={MyStrings.editModal.placeholder}
             text={nickname}
             onChangeText={onChangeText}
             onPressClear={onPressClear}
+            maxLength={NICKNAME_VALIDATION.MAX_LENGTH}
           />
         </View>
         <ModalButtonLayout
+          disable={disable}
           onPressCancle={closeEditModal}
           onPressConfirm={handleUpdateNickname}
         />

14e39999bd24c5f98e28322916530f46f72962e0
Leeseoleem <leeseorim0029@gmail.com>
2025-06-12 16:56:36 +0900

    feat: 유저 확인 및 로그인 리디렉션 유틸 함수 추가

---
+++ quizlab/constants/auth/authErrorMessages.ts
@@ -0,0 +1,3 @@
+export const AuthErrorMessages = {
+  noUser: "로그인된 유저가 없습니다",
+};
--- quizlab/lib/utils/fetchCurrentUserInfo.ts
+++ quizlab/lib/utils/fetchCurrentUserInfo.ts
@@ -1,4 +1,8 @@
 import { auth } from "../firebase/config";
+import { router } from "expo-router";
+import { ROUTES } from "@/constants/routes";
+import { showToast } from "./toastMessage";
+import { AuthErrorMessages } from "@/constants/auth/authErrorMessages";
 
 export const fetchCurrentUserUid = async () => {
   const user = auth.currentUser;
@@ -19,4 +23,15 @@ export const fetchCurrentUserInfo = async () => {
     // 아직 로그인 정보가 준비 안 된 상태일 수도 있음
     return null;
   }
+};
+
+// 유저 인증
+export const getCurrentUserOrRedirect = () => {
+  const user = auth.currentUser;
+  if (!user) {
+    showToast(AuthErrorMessages.noUser);
+    router.replace(ROUTES.LOGIN); // 또는 ONBOARDING 등
+    return null;
+  }
+  return user;
 };

0f1262220e7fa34f7a74978aec7e36e176dd2f3a
Leeseoleem <leeseorim0029@gmail.com>
2025-06-20 22:58:29 +0900

    feat: auth 함수 추가

---
+++ quizlab/lib/utils/auth/auth.ts
@@ -0,0 +1,24 @@
+import { supabase } from "@/lib/supabase";
+
+// 회원가입
+export const signUp = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signUp({ email, password });
+  if (error) throw error;
+  return data;
+};
+
+// 로그인
+export const signIn = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw error;
+  return data;
+};
+
+// 로그아웃
+export const signOut = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw error;
+};

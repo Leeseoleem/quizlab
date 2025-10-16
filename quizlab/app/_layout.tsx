0ed78d145befcdfdae63824f4353579fb2e693c3
Leeseoleem <leeseorim0029@gmail.com>
2025-10-17 03:14:42 +0900

    chore: Pretendard 폰트 useFonts 설정 추가

--- quizlab/app/_layout.tsx
+++ quizlab/app/_layout.tsx
@@ -3,13 +3,42 @@ import "react-native-reanimated";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 import "../global.css";
+import { useEffect } from "react";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
+// 스플래시 스크린이 자동으로 사라지지 않도록 설정
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  // 비동기적으로 폰트를 로드
+  const [fontsLoaded] = useFonts({
+    "Pretendard-Black": require("../assets/fonts/Pretendard-Black.ttf"),
+    "Pretendard-ExtraBold": require("../assets/fonts/Pretendard-ExtraBold.ttf"),
+    "Pretendard-Bold": require("../assets/fonts/Pretendard-Bold.ttf"),
+    "Pretendard-SemiBold": require("../assets/fonts/Pretendard-SemiBold.ttf"),
+    "Pretendard-Medium": require("../assets/fonts/Pretendard-Medium.ttf"),
+    "Pretendard-Regular": require("../assets/fonts/Pretendard-Regular.ttf"),
+    "Pretendard-Light": require("../assets/fonts/Pretendard-Light.ttf"),
+    "Pretendard-ExtraLight": require("../assets/fonts/Pretendard-ExtraLight.ttf"),
+    "Pretendard-Thin": require("../assets/fonts/Pretendard-Thin.ttf"),
+  });
+
+  // 폰트가 모두 로드되면 스플래시 스크린을 숨김
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // 폰트가 로드되지 않았으면 아무것도 렌더링하지 않음
+  if (!fontsLoaded) return null;
+
   return (
     <SafeAreaProvider>
       <Stack>

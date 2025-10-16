0470c1d0d75cce28af67117d893e2e32a873fde3
Leeseoleem <leeseorim0029@gmail.com>
2025-10-17 03:17:01 +0900

    style: Pretendard 폰트 패밀리 설정 및 커스텀 폰트 플러그인 추가

--- quizlab/tailwind.config.ts
+++ quizlab/tailwind.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 import {
   BaseColors,
@@ -22,6 +23,17 @@ const config: Config = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      fontFamily: {
+        "pretendard-black": ["Pretendard-Black"],
+        "pretendard-extrabold": ["Pretendard-ExtraBold"],
+        "pretendard-bold": ["Pretendard-Bold"],
+        "pretendard-semibold": ["Pretendard-SemiBold"],
+        "pretendard-medium": ["Pretendard-Medium"],
+        "pretendard-regular": ["Pretendard-Regular"],
+        "pretendard-light": ["Pretendard-Light"],
+        "pretendard-extralight": ["Pretendard-ExtraLight"],
+        "pretendard-thin": ["Pretendard-Thin"],
+      },
       colors: {
         brand: BaseColors.brand,
         brandLight: BaseColors.brandLight,
@@ -36,7 +48,61 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      // 폰트 플러그인 추가
+      addComponents({
+        ".text-header": {
+          fontFamily: "Pretendard-Bold",
+          fontSize: "28px",
+          lineHeight: "34px",
+          letterSpacing: "-0.7px",
+        },
+        ".text-title-01": {
+          fontFamily: "Pretendard-Bold",
+          fontSize: "24px",
+          lineHeight: "30px",
+          letterSpacing: "-0.6px",
+        },
+        ".text-title-02": {
+          fontFamily: "Pretendard-SemiBold",
+          fontSize: "20px",
+          lineHeight: "25px",
+          letterSpacing: "-0.5px",
+        },
+        ".text-subtitle": {
+          fontFamily: "Pretendard-SemiBold",
+          fontSize: "16px",
+          lineHeight: "21px",
+          letterSpacing: "-0.4px",
+        },
+        ".text-body": {
+          fontFamily: "Pretendard-Medium",
+          fontSize: "16px",
+          lineHeight: "21px",
+          letterSpacing: "-0.4px",
+        },
+        ".text-caption": {
+          fontFamily: "Pretendard-Medium",
+          fontSize: "14px",
+          lineHeight: "19px",
+          letterSpacing: "-0.35px",
+        },
+        ".text-description": {
+          fontFamily: "Pretendard-Medium",
+          fontSize: "12px",
+          lineHeight: "16px",
+          letterSpacing: "-0.3px",
+        },
+        ".text-micro": {
+          fontFamily: "Pretendard-Regular",
+          fontSize: "8px",
+          lineHeight: "16px",
+          letterSpacing: "-0.2px",
+        },
+      });
+    }),
+  ],
 };
 
 export default config;

0952cd786e9298dc1577323968b5a35e94873439
Leeseoleem <leeseorim0029@gmail.com>
2025-10-17 03:50:40 +0900

    chore: 테스트 용 코드

--- quizlab/app/(tabs)/setting/index.tsx
+++ quizlab/app/(tabs)/setting/index.tsx
@@ -5,10 +5,8 @@ export default function SettingScreen() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
       <View className="p-4 bg-yellow-200 rounded-lg">
-        <Text className="text-xl font-bold text-yellow-900">설정 페이지</Text>
-        <Text className="mt-2 text-yellow-800">
-          여기에 설정 관련 내용을 추가하세요.
-        </Text>
+        <Text className="text-header">테스트</Text>
+        <Text className="text-title-01">테스트</Text>
       </View>
     </SafeAreaView>
   );

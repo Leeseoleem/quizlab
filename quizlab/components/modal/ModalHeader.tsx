4060243d6693c718d3677dc3479a495dd73bd824
Leeseoleem <leeseorim0029@gmail.com>
2025-10-23 13:45:20 +0900

    feat: 모달 전용 헤더 컴포넌트 생성

---
+++ quizlab/components/modal/ModalHeader.tsx
@@ -0,0 +1,44 @@
+import { View, TouchableOpacity, Text } from "react-native";
+import { clsx } from "clsx";
+
+import Feather from "@expo/vector-icons/Feather";
+import { Gray } from "@/constants/colors";
+
+type Variant = "back" | "close";
+
+interface ModalHeaderProps {
+  variant: Variant;
+  label?: string;
+  onPressBack?: () => void;
+  onPressClose?: () => void;
+}
+
+const ModalHeader = ({
+  variant,
+  label,
+  onPressBack,
+  onPressClose,
+}: ModalHeaderProps) => {
+  return (
+    <View className="w-full flex flex-row h-[56px] items-center p-6 border-b border-gray-5">
+      {variant === "back" && (
+        <View className="flex flex-row w-full justify-start items-center">
+          <TouchableOpacity onPress={onPressBack} activeOpacity={0.8}>
+            <Feather name="chevron-left" size={16} color={Gray.black} />
+          </TouchableOpacity>
+          <Text className="text-body text-gray-black">{label}</Text>
+        </View>
+      )}
+      {variant === "close" && (
+        <View className="flex flex-row w-full justify-between items-center">
+          <Text className="text-body text-gray-black">{label}</Text>
+          <TouchableOpacity onPress={onPressClose} activeOpacity={0.8}>
+            <Feather name="x" size={16} color={Gray.black} />
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default ModalHeader;

5abd4037642e8f7910a071869297aa4fb8b3a339
Leeseoleem <leeseorim0029@gmail.com>
2025-10-31 03:37:26 +0900

    style: 모달 전용 헤더 디자인 내용 수정
    - 모달 패딩 값: 20px로 수정
    - 아이콘 크기 수정

--- quizlab/components/modal/ModalHeader.tsx
+++ quizlab/components/modal/ModalHeader.tsx
@@ -19,11 +19,11 @@ const ModalHeader = ({
   onPressClose,
 }: ModalHeaderProps) => {
   return (
-    <View className="w-full flex flex-row h-[56px] items-center p-6 border-b border-gray-5">
+    <View className="w-full flex flex-row h-[56px] px-5 items-center border-b border-gray-5">
       {variant === "back" && (
-        <View className="flex flex-row w-full justify-start items-center">
+        <View className="flex flex-row w-full justify-start items-center gap-5">
           <TouchableOpacity onPress={onPressBack} activeOpacity={0.8}>
-            <Feather name="chevron-left" size={16} color={Gray.black} />
+            <Feather name="chevron-left" size={20} color={Gray.black} />
           </TouchableOpacity>
           <Text className="text-body text-gray-black">{label}</Text>
         </View>
@@ -32,7 +32,7 @@ const ModalHeader = ({
         <View className="flex flex-row w-full justify-between items-center">
           <Text className="text-body text-gray-black">{label}</Text>
           <TouchableOpacity onPress={onPressClose} activeOpacity={0.8}>
-            <Feather name="x" size={16} color={Gray.black} />
+            <Feather name="x" size={20} color={Gray.black} />
           </TouchableOpacity>
         </View>
       )}

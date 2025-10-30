9fb9fc8d43fc9c149b35d0a2a5dc9bff7aa8988f
Leeseoleem <leeseorim0029@gmail.com>
2025-10-30 16:26:41 +0900

    style: 닫기 헤더 컴포넌트 스타일 누락

--- quizlab/components/common/Header/CloseHeader.tsx
+++ quizlab/components/common/Header/CloseHeader.tsx
@@ -2,6 +2,7 @@ import { TouchableOpacity, View } from "react-native";
 import HeaderContainer from "./HeaderContainer";
 
 import Feather from "@expo/vector-icons/Feather";
+import { Gray } from "@/constants/colors";
 
 interface CloseHeaderProps {
   onPressClose: () => void;
@@ -13,8 +14,8 @@ const CloseHeader = ({ onPressClose, children }: CloseHeaderProps) => {
     <HeaderContainer>
       <View className="flex flex-row w-full items-center justify-between">
         <View>{children}</View>
-        <TouchableOpacity onPress={onPressClose}>
-          <Feather name="x" size={24} color="black" />
+        <TouchableOpacity onPress={onPressClose} activeOpacity={0.8}>
+          <Feather name="x" size={24} color={Gray.black} />
         </TouchableOpacity>
       </View>
     </HeaderContainer>

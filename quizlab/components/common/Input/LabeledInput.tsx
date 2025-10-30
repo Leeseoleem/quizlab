5e2a506c2324c41dbec2523e114e28197503c38f
Leeseoleem <leeseorim0029@gmail.com>
2025-10-30 12:37:53 +0900

    feat: 라벨 포함 Input Container 컴포넌트 생성

---
+++ quizlab/components/common/Input/LabeledInput.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+
+interface LabeledInputContainerProps {
+  label: string;
+  children: React.ReactNode;
+}
+
+const LabeledInputContainer = ({
+  label,
+  children,
+}: LabeledInputContainerProps) => {
+  return (
+    <View className="flex flex-col w-full justify-start gap-1">
+      <Text className="text-description text-gray-80 ml-1">{label}</Text>
+      {children}
+    </View>
+  );
+};
+
+export default LabeledInputContainer;

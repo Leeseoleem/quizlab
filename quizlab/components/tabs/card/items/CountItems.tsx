f97e40a0535ebddf5580a16d54c6ec7898d3dbdc
Leeseoleem <leeseorim0029@gmail.com>
2025-11-10 16:12:07 +0900

    chore: Count 아이템 사이즈 옵션 추가 및 스타일 확장

--- quizlab/components/tabs/card/items/CountItems.tsx
+++ quizlab/components/tabs/card/items/CountItems.tsx
@@ -1,27 +1,38 @@
 import { View, Text } from "react-native";
 import { clsx } from "clsx";
 import { BaseColors } from "@/constants/colors";
+import type { SizeType_A } from "@/types/common.types";
 
 import Feather from "@expo/vector-icons/Feather";
 
 interface CountItemsProps {
+  size: SizeType_A;
   type: "correct" | "incorrect";
   count: number;
 }
 
 export const CountItems = (props: CountItemsProps) => {
-  const { type, count } = props;
+  const { size, type, count } = props;
 
   const isCorrect = type === "correct";
   const iconName = isCorrect ? "check-circle" : "x-circle";
   const iconColor = isCorrect ? BaseColors.success : BaseColors.danger; // 초록색 / 빨간색
 
   return (
-    <View className="flex flex-row justify-center items-center gap-1">
-      <Feather name={iconName} size={14} color={iconColor} />
+    <View
+      className={clsx(
+        "flex flex-row justify-center items-center",
+        size === "default" ? "gap-1" : "gap-2"
+      )}
+    >
+      <Feather
+        name={iconName}
+        size={size === "default" ? 14 : 20}
+        color={iconColor}
+      />
       <Text
         className={clsx(
-          "text-description",
+          size === "default" ? "text-description" : "text-body",
           isCorrect ? "text-success" : "text-danger"
         )}
       >

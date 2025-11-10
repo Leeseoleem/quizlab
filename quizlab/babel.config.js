00ab32abf6fdff8d8cd655a805db805f0edbe5bb
Leeseoleem <leeseorim0029@gmail.com>
2025-11-10 19:01:29 +0900

    chore: dotenv 관련 라이브러리 제거
    - 해당 라이브러리와 expo-router 간의 충돌 발생 => expo 공식에서 권장하는 방식으로 수정 예정

--- quizlab/babel.config.js
+++ quizlab/babel.config.js
@@ -20,7 +20,6 @@ module.exports = function (api) {
           },
         },
       ],
-      ["module:react-native-dotenv", { moduleName: "@env", path: ".env" }],
       "react-native-reanimated/plugin",
     ],
   };

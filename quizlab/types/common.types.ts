c5b85cf6f8d34202b762ee711dbcbed694801e25
Leeseoleem <leeseorim0029@gmail.com>
2025-11-11 17:32:53 +0900

    chore: 상태 타입 추가

--- quizlab/types/common.types.ts
+++ quizlab/types/common.types.ts
@@ -5,10 +5,15 @@
  * @typedef {("default" | "large")} SizeType_A
  * @typedef {("default" | "small")} SizeType_B
  */
-
 export type SizeType = "small" | "medium" | "large";
 export type SizeType_A = "default" | "large";
 export type SizeType_B = "default" | "small";
+
+/**
+ * 상태 타입
+ */
+
+export type Status = "default" | "danger";
 
 // 풀이 모드
 export type SolvedMode = "timed" | "free";

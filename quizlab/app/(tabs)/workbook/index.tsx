ada0d92f5b6ca334d4e8102214373ee70b5c34aa
Leeseoleem <leeseorim0029@gmail.com>
2025-11-16 02:09:12 +0900

    chore: AnimatePresence 자식 요소에 key props 추가

--- quizlab/app/(tabs)/workbook/index.tsx
+++ quizlab/app/(tabs)/workbook/index.tsx
@@ -256,6 +256,7 @@ export default function WorkbookScreen() {
           <AnimatePresence exitBeforeEnter>
             {showScrollButton && (
               <MotiView
+                key="scroll-to-top-button"
                 from={{ opacity: 0, translateY: 12 }}
                 animate={{ opacity: 1, translateY: 0 }}
                 exit={{ opacity: 0, translateY: 12 }}

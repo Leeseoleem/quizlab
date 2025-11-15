967dbf1363812f42bf9965e660dbd8bc0df8d4b0
Leeseoleem <leeseorim0029@gmail.com>
2025-11-15 23:48:47 +0900

    feat: 문제집 페이지 텍스트 리소스 추가

---
+++ quizlab/constants/texts/workbook.ts
@@ -0,0 +1,28 @@
+export const workbookTexts = {
+  main: {
+    title: "문제집",
+    placeholder: "문제집을 검색해보세요!",
+    emptyText: "검색 결과가 없습니다.\n다른 검색어를 시도해보세요.",
+    toggle: [
+      { id: "all", label: "전체" },
+      { id: "color", label: "색상별" },
+    ],
+    colorPickerModal: {
+      headerLabel: "색상 선택하기",
+      selectButtonLabel: "선택하기",
+      cancelButtonLabel: "취소",
+    },
+    addWorkbookModal: {
+      headerLabel: "새 문제집",
+      titleLabel: "문제집 이름",
+      titlePlaceholder: "문제집 이름을 입력하세요",
+      descriptionLabel: "문제집 설명",
+      descriptionPlaceholder:
+        "문제집에 대한 간단한 설명을 입력하세요 (선택 사항)",
+      colorSelectLabel: "문제집 색상 선택하기",
+      nextButtonLabel: "다음",
+      addButtonLabel: "문제집 추가하기",
+      cancelButtonLabel: "취소",
+    },
+  },
+};

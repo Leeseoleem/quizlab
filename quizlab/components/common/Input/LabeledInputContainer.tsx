8801b7589c9080ecd0328b349ac15083bfe5ade3
Leeseoleem <leeseorim0029@gmail.com>
2025-11-15 23:31:57 +0900

    feat: input 컴포넌트에 필수 여부 추가
    - 필수일 경우, 라벨 옆에 * 표시 출력

--- quizlab/components/common/Input/LabeledInputContainer.tsx
+++ quizlab/components/common/Input/LabeledInputContainer.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import { View, Text } from "react-native";
 
 interface LabeledInputContainerProps {
+  isRequired?: boolean;
   label: string;
   children: React.ReactNode;
 }
 
 const LabeledInputContainer = ({
+  isRequired = false,
   label,
   children,
 }: LabeledInputContainerProps) => {
   return (
     <View className="flex flex-col w-full justify-start gap-1">
-      <Text className="text-description text-gray-80 ml-1">{label}</Text>
+      <View className="flex flex-row gap-1 justify-start items-start">
+        <Text className="text-description text-gray-80">{label}</Text>
+        {isRequired && <Text className="text-description text-danger">*</Text>}
+      </View>
       {children}
     </View>
   );

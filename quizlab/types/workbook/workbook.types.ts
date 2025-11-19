17ce1d3462767b9e21a4971fe1d19dcf70444a6f
Leeseoleem <leeseorim0029@gmail.com>
2025-11-19 15:51:47 +0900

    chore: 파라미터 값 수정에 따른 타입 조정

--- quizlab/types/workbook/workbook.types.ts
+++ quizlab/types/workbook/workbook.types.ts
@@ -12,3 +12,23 @@ export type Workbook = {
   color: FolderColorKey; // 색상 키 (예: "teal" | "sky" | ...)
   totalCount: number; // 폴더 내 총 문제 수
 };
+
+/**
+ * 문제집 상세 페이지 파라미터
+ */
+export type RawParams = {
+  id?: string | string[];
+  title?: string | string[];
+  description?: string | string[];
+  color?: string | string[];
+  totalCount?: string | string[];
+};
+
+// 문제집 파라미터 파라미터 타입 정의
+export type WorkbookParams = {
+  id: string;
+  title?: string;
+  description?: string;
+  color?: FolderColorKey;
+  totalCount?: number;
+};

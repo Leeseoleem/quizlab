7128df95d2cec1c8f5c0ddb1370da2bed7e12639
Leeseoleem <leeseorim0029@gmail.com>
2025-11-21 12:25:47 +0900

    chore: 각 컨텐츠 영역 type export

--- quizlab/components/tabs/workbook/detail/modal/contents/index.ts
+++ quizlab/components/tabs/workbook/detail/modal/contents/index.ts
@@ -1,2 +1,4 @@
 export { DescriptiveContent } from "./DescriptiveContent";
+export type { DescriptiveContentProps } from "./DescriptiveContent";
 export { ChoiceContent } from "./ChoiceContent";
+export type { ChoiceContentProps } from "./ChoiceContent";

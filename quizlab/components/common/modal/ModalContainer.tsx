682d4e516f572bc461508dd3a3cd8301b2ff3d99
Leeseoleem <leeseorim0029@gmail.com>
2025-11-21 12:21:25 +0900

    refactor: 모달 컨테이너 input 키보드 위치 대응

--- quizlab/components/common/modal/ModalContainer.tsx
+++ quizlab/components/common/modal/ModalContainer.tsx
@@ -1,4 +1,5 @@
 import { Pressable, View, Modal } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import ModalHeader from "./ModalHeader";
 import type { ModalHeaderProps } from "./ModalHeader";
@@ -39,41 +40,56 @@ const ModalContainer = ({ contents, header, footer }: ModalContainerProps) => {
       visible={contents.visible} // 모달 표시 여부
       onRequestClose={contents.onClose} // 안드로이드 백 버튼 처리
     >
-      {/* 오버레이 영역 */}
-      <Pressable
-        className="flex flex-1 justify-center items-center p-4 bg-gray-black/50"
-        onPress={contents.onClose}
+      {/* 키보드까지 고려하는 스크롤 컨테이너 */}
+      <KeyboardAwareScrollView
+        // 화면 전체를 덮는 컨테이너로 만들기
+        className="flex bg-gray-black/50"
+        contentContainerStyle={{ flexGrow: 1 }}
+        enableOnAndroid
+        extraScrollHeight={24}
+        keyboardShouldPersistTaps="handled"
       >
-        {/* 모달 콘텐츠 영역 */}
-        <Pressable onPress={(e) => e.stopPropagation()}>
-          <View className="flex bg-gray-white rounded-lg overflow-hidden">
-            <ModalHeader
-              variant={header.variant}
-              label={header.label}
-              onPressBack={header.onPressBack}
-              onPressClose={header.onPressClose}
-            />
-            <View>{contents.children}</View>
-            <View>
-              {footer && (
-                <View className="flex w-full p-5 gap-3">
-                  <CommonButton
-                    isDisabled={footer.isConfirmDisabled}
-                    label={footer.confirmLabel ?? "확인"}
-                    onPress={footer.onConfirm}
-                  />
-                  <CommonButton
-                    isDisabled={footer.isCancelDisabled}
-                    variant="secondary"
-                    label={footer.cancelLabel ?? "취소"}
-                    onPress={footer.onCancel}
-                  />
-                </View>
-              )}
+        {/* 오버레이 영역 */}
+        <Pressable
+          className="flex-1 justify-center items-center p-4"
+          onPress={contents.onClose}
+        >
+          {/* 모달 콘텐츠 영역 */}
+          <Pressable onPress={(e) => e.stopPropagation()}>
+            <View className="flex bg-gray-white rounded-lg overflow-hidden">
+              {/* 헤더 */}
+              <ModalHeader
+                variant={header.variant}
+                label={header.label}
+                onPressBack={header.onPressBack}
+                onPressClose={header.onPressClose}
+              />
+
+              {/* 본문 */}
+              <View className="max-h-[60vh]">{contents.children}</View>
+
+              {/* 푸터 */}
+              <View className="flex">
+                {footer && (
+                  <View className="flex w-full p-5 gap-3">
+                    <CommonButton
+                      isDisabled={footer.isConfirmDisabled}
+                      label={footer.confirmLabel ?? "확인"}
+                      onPress={footer.onConfirm}
+                    />
+                    <CommonButton
+                      isDisabled={footer.isCancelDisabled}
+                      variant="secondary"
+                      label={footer.cancelLabel ?? "취소"}
+                      onPress={footer.onCancel}
+                    />
+                  </View>
+                )}
+              </View>
             </View>
-          </View>
+          </Pressable>
         </Pressable>
-      </Pressable>
+      </KeyboardAwareScrollView>
     </Modal>
   );
 };

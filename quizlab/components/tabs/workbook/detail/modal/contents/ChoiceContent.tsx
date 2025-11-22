8a0b01b474c8783b94359f827f80da25ae481ba3
Leeseoleem <leeseorim0029@gmail.com>
2025-11-23 02:53:22 +0900

    f 문제 추가 모달- 선택형 탭 화면 구현

--- quizlab/components/tabs/workbook/detail/modal/contents/ChoiceContent.tsx
+++ quizlab/components/tabs/workbook/detail/modal/contents/ChoiceContent.tsx
@@ -1,9 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import ProblemBodySection from "./ProblemBodySection";
+import type { ProblemBodySectionProps } from "./ProblemBodySection";
+import AddOptionButton from "../../button/AddOptionButton";
+import ChoiceOptionInput from "../../ChoiceOptionInput";
+import type { ChoiceOptionInputProps } from "../../ChoiceOptionInput";
+import { workbookTexts } from "@/constants/texts/workbook";
 
-export const ChoiceContent = () => {
+export interface ChoiceFooterProps {
+  onAddOption: () => void;
+  optionList: ChoiceOptionInputProps[];
+}
+export interface ChoiceContentProps {
+  body: ProblemBodySectionProps;
+  footer: ChoiceFooterProps;
+}
+
+export const ChoiceContent = ({ body, footer }: ChoiceContentProps) => {
   return (
-    <View>
-      <Text className="mt-4 text-gray-700">선택형</Text>
+    <View className="flex p-5 gap-5">
+      <ProblemBodySection {...body} />
+      <View className="flex gap-3">
+        <View className="flex-row justify-between items-center">
+          <Text className="text-caption text-gray-80">
+            {workbookTexts.detail.addProblemModal.choiceOptionLabel}
+          </Text>
+          <AddOptionButton
+            label={workbookTexts.detail.addProblemModal.addOptionButtonLable}
+            onAddOption={footer.onAddOption}
+          />
+        </View>
+        {/* 선택지 리스트 스크롤 영역 */}
+        <ScrollView
+          // 최대 높이를 제한해서, 선택지가 많아지면 이 안에서만 스크롤
+          className="max-h-48" // 필요에 따라 48, 56 등으로 조절
+          // 안쪽 아이템들 간격
+          contentContainerStyle={{ gap: 12, paddingBottom: 8 }}
+          // 키보드가 떠 있을 때도 터치 가능하게
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+          {footer.optionList.map((item, index) => (
+            <ChoiceOptionInput key={index} {...item} />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 };

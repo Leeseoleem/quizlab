8d50fe46871e2e85ffa59ef3c362519272faa7e9
Leeseoleem <leeseorim0029@gmail.com>
2025-11-23 00:45:32 +0900

    feat: 선택지 옵션 추가 버튼 컴포넌트 생성

--- quizlab/components/tabs/workbook/detail/button/AddOptionButton.tsx
+++ quizlab/components/tabs/workbook/detail/button/AddOptionButton.tsx
@@ -1,13 +1,21 @@
 import { Pressable, Text } from "react-native";
 
+import { Gray } from "@/constants/colors";
+import Octicons from "@expo/vector-icons/Octicons";
+
 interface AddOptionButtonProp {
   label: string;
+  onAddOption: () => void;
 }
 
-const AddOptionButton = ({ label }: AddOptionButtonProp) => {
+const AddOptionButton = ({ label, onAddOption }: AddOptionButtonProp) => {
   return (
-    <Pressable className="flex">
-      <Text>{label}</Text>
+    <Pressable
+      className="flex-row py-3 px-2 items-center justify-center gap-2 rounded-3xl bg-gray-5"
+      onPress={onAddOption}
+    >
+      <Octicons name="plus" size={14} color={Gray.black} />
+      <Text className="text-caption text-gray-black">{label}</Text>
     </Pressable>
   );
 };

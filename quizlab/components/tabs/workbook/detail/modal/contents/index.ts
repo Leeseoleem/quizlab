a321ba34fe8a601eeeaa6e2f724fcf6d6092724b
Leeseoleem <leeseorim0029@gmail.com>
2025-11-23 02:54:09 +0900

    chore: 선택형 화면 관련 export 추가

--- quizlab/components/tabs/workbook/detail/modal/contents/index.ts
+++ quizlab/components/tabs/workbook/detail/modal/contents/index.ts
@@ -1,4 +1,9 @@
+export type { ProblemBodySectionProps } from "./ProblemBodySection";
+// 각 문제 유형별 영역
 export { DescriptiveContent } from "./DescriptiveContent";
-export type { DescriptiveContentProps } from "./DescriptiveContent";
+export type {
+  DescriptiveFooterProps,
+  DescriptiveContentProps,
+} from "./DescriptiveContent";
 export { ChoiceContent } from "./ChoiceContent";
-export type { ChoiceContentProps } from "./ChoiceContent";
+export type { ChoiceFooterProps, ChoiceContentProps } from "./ChoiceContent";

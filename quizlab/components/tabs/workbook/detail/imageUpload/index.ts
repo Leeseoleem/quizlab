a16bc9d501525c46cc2744705d3ae8de4974b321
Leeseoleem <leeseorim0029@gmail.com>
2025-11-22 22:07:58 +0900

    chore: 이미지 선택 관련 아이템 export 파일

---
+++ quizlab/components/tabs/workbook/detail/imageUpload/index.ts
@@ -0,0 +1,4 @@
+export { AddImageButton } from "./AddImageButton";
+export type { AddImageButtonProps } from "./AddImageButton";
+export { UploadedImageItem } from "./UploadedImageItem";
+export type { UploadedImageItemProps } from "./UploadedImageItem";

b1e460d8831ebec7f573d8b15d2e1e16f2d64cf1
Leeseoleem <leeseorim0029@gmail.com>
2025-11-22 22:53:54 +0900

    feat: ImageUploadContainer 영역 구현
    - 이미지 추가 버튼 + 추가된 이미지 리스트
    - 이미지 리스트는 스크롤뷰를 통해 전채 확인이 가능

---
+++ quizlab/components/tabs/workbook/detail/imageUpload/ImageUploadContainer.tsx
@@ -0,0 +1,47 @@
+import { View, ScrollView } from "react-native";
+import LabeledInputContainer from "@/components/common/Input/LabeledInputContainer";
+import { AddImageButton, UploadedImageItem } from "./index";
+
+export interface ImageUploadContainerProps {
+  images: string[]; // 현재 업로드된 이미지 uri 리스트
+  onAddImage: () => void; // 이미지 추가 버튼 클릭 시 호출
+  onRemoveImage: (index: number) => void; // 특정 이미지 삭제 (index 기반)
+}
+
+const ImageUploadContainer = ({
+  images,
+  onAddImage,
+  onRemoveImage,
+}: ImageUploadContainerProps) => {
+  const imageCount = images.length;
+
+  return (
+    <LabeledInputContainer label="이미지 추가 (선택 사항)">
+      <View className="flex-row w-full gap-2 items-center">
+        {/* 이미지 추가 버튼 */}
+        <AddImageButton imageCount={imageCount} onAddImage={onAddImage} />
+        <ScrollView
+          horizontal // 가로 스크롤 활성화
+          nestedScrollEnabled // 스크롤뷰 중첩
+          showsHorizontalScrollIndicator={false} // 하단 스크롤바 숨기기
+          style={{
+            flex: 1,
+          }}
+        >
+          {/* 업로드된 이미지 목록 */}
+          <View className="flex-row gap-2">
+            {images.map((uri, index) => (
+              <UploadedImageItem
+                key={index}
+                uri={uri}
+                onRemove={() => onRemoveImage(index)}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    </LabeledInputContainer>
+  );
+};
+
+export default ImageUploadContainer;

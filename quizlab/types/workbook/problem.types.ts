a804b2439fc5391c5b3bd227c06b5a5dbf81bcd4
Leeseoleem <leeseorim0029@gmail.com>
2025-11-24 02:22:22 +0900

    chore: 문제 풀이 모드 용 타입 추가

--- quizlab/types/workbook/problem.types.ts
+++ quizlab/types/workbook/problem.types.ts
@@ -2,3 +2,21 @@
  * 문제 타입 정의
  */
 export type ProblemType = "descriptive" | "choice";
+export type SolvedModeType = "timed" | "free";
+
+export const SolvedModeTexts: Record<
+  SolvedModeType,
+  {
+    title: string;
+    description: string;
+  }
+> = {
+  timed: {
+    title: "시간 제한 모드",
+    description: "제한 시간 내에 문제를 풀어보세요",
+  },
+  free: {
+    title: "자유 모드",
+    description: "자유롭게 문제를 풀어보세요",
+  },
+};

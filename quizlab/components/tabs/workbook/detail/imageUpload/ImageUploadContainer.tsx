6d4461d929af5aeb42043e87c4b10f6766918de6
Leeseoleem <leeseorim0029@gmail.com>
2025-11-23 15:30:07 +0900

    stlye: 이미지 추가 영역 토글로 수정
    - 이미지 추가는 필수가 아닌 선택사항 이기 때문에 토글 처리를 통해 숨겨놓을 수 있도록 설정

--- quizlab/components/tabs/workbook/detail/imageUpload/ImageUploadContainer.tsx
+++ quizlab/components/tabs/workbook/detail/imageUpload/ImageUploadContainer.tsx
@@ -1,7 +1,13 @@
-import { View, ScrollView } from "react-native";
+import { useState } from "react";
+import { MotiView, AnimatePresence } from "moti";
+import { View, Text, Pressable, ScrollView } from "react-native";
+import { clsx } from "clsx";
+
 import { workbookTexts } from "@/constants/texts/workbook";
-import LabeledInputContainer from "@/components/common/Input/LabeledInputContainer";
 import { AddImageButton, UploadedImageItem } from "./index";
+
+import { Gray } from "@/constants/colors";
+import Octicons from "@expo/vector-icons/Octicons";
 
 export interface ImageUploadContainerProps {
   images: string[]; // 현재 업로드된 이미지 uri 리스트
@@ -16,34 +22,82 @@ const ImageUploadContainer = ({
 }: ImageUploadContainerProps) => {
   const imageCount = images.length;
 
+  const [isImageSectionOpen, setIsImageSectionOpen] = useState(false);
+  const toggleImageSection = () => {
+    setIsImageSectionOpen((prev) => !prev);
+  };
+
+  const toggleLabelClass = clsx(
+    "text-caption",
+    isImageSectionOpen ? "text-gray-80" : "text-gray-40"
+  );
+
   return (
-    <LabeledInputContainer
-      label={workbookTexts.detail.addProblemModal.imageUploadLabel}
-    >
-      <View className="flex-row w-full gap-2 items-center">
-        {/* 이미지 추가 버튼 */}
-        <AddImageButton imageCount={imageCount} onAddImage={onAddImage} />
-        <ScrollView
-          horizontal // 가로 스크롤 활성화
-          nestedScrollEnabled // 스크롤뷰 중첩
-          showsHorizontalScrollIndicator={false} // 하단 스크롤바 숨기기
-          style={{
-            flex: 1,
-          }}
-        >
-          {/* 업로드된 이미지 목록 */}
-          <View className="flex-row gap-2">
-            {images.map((uri, index) => (
-              <UploadedImageItem
-                key={index}
-                uri={uri}
-                onRemove={() => onRemoveImage(index)}
-              />
-            ))}
-          </View>
-        </ScrollView>
-      </View>
-    </LabeledInputContainer>
+    <View className="flex flex-col w-full justify-start gap-1">
+      <Pressable
+        onPress={toggleImageSection}
+        className="flex-row py-1 items-center justify-between"
+      >
+        <Text className={toggleLabelClass}>
+          {workbookTexts.detail.addProblemModal.imageUploadLabel}
+        </Text>
+        {isImageSectionOpen ? (
+          <Octicons name="triangle-down" size={24} color={Gray[60]} />
+        ) : (
+          <Octicons name="triangle-left" size={24} color={Gray[60]} />
+        )}
+      </Pressable>
+      {/* 나타나고/사라질 때 애니메이션 */}
+      <AnimatePresence>
+        {isImageSectionOpen && (
+          <MotiView
+            // 처음 나타날 때 상태
+            from={{
+              opacity: 0,
+              translateY: -8,
+            }}
+            // 애니메이션 후 최종 상태
+            animate={{
+              opacity: 1,
+              translateY: 0,
+            }}
+            // 사라질 때 상태
+            exit={{
+              opacity: 0,
+              translateY: -8,
+            }}
+            transition={{
+              type: "timing",
+              duration: 300,
+            }}
+          >
+            <View className="flex-row w-full gap-2 items-center">
+              {/* 이미지 추가 버튼 */}
+              <AddImageButton imageCount={imageCount} onAddImage={onAddImage} />
+              <ScrollView
+                horizontal // 가로 스크롤 활성화
+                nestedScrollEnabled // 스크롤뷰 중첩
+                showsHorizontalScrollIndicator={false} // 하단 스크롤바 숨기기
+                style={{
+                  flex: 1,
+                }}
+              >
+                {/* 업로드된 이미지 목록 */}
+                <View className="flex-row gap-2">
+                  {images.map((uri, index) => (
+                    <UploadedImageItem
+                      key={index}
+                      uri={uri}
+                      onRemove={() => onRemoveImage(index)}
+                    />
+                  ))}
+                </View>
+              </ScrollView>
+            </View>
+          </MotiView>
+        )}
+      </AnimatePresence>
+    </View>
   );
 };
 

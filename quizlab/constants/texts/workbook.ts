e28f7a3da1ff50525ae93dfa9ba22953c94120bb
Leeseoleem <leeseorim0029@gmail.com>
2025-11-25 05:21:20 +0900

    chore: 문제집 상세 페이지 전용 텍스트 분리

--- quizlab/constants/texts/workbook.ts
+++ quizlab/constants/texts/workbook.ts
@@ -1,4 +1,8 @@
 export const workbookTexts = {
+  popover: {
+    edit: `수정하기`,
+    delete: `삭제하기`,
+  },
   main: {
     title: "문제집",
     placeholder: "문제집을 검색해보세요!",
@@ -9,10 +13,7 @@ export const workbookTexts = {
       { id: "all", label: "전체" },
       { id: "color", label: "색상별" },
     ],
-    popover: {
-      edit: `수정하기`,
-      delete: `삭제하기`,
-    },
+
     colorPickerModal: {
       headerLabel: "색상 선택하기",
       selectButtonLabel: "선택하기",
@@ -33,5 +34,33 @@ export const workbookTexts = {
   },
   detail: {
     header: "목록으로",
+    emptyText: "아직 등록된 문제가 없습니다.\n새로운 문제를 추가해보세요.",
+    addProblemModal: {
+      headerLabel: "문제 추가하기",
+      imageUploadLabel: "이미지 추가 (선택 사항)",
+      problemlInputLabel: "문제",
+      problemlInputPlaceholder: "문제를 입력하세요",
+      answerInputLabel: "정답",
+      answerInputPlaceholder: "정답을 입력하세요",
+      optionInputPlaceholder: "내용을 입력하세요",
+      choiceOptionLabel: "선택지",
+      addButtonLabel: "추가하기",
+      cancelButtonLabel: "취소",
+      addOptionButtonLable: "선택지 추가",
+      toastMessage: {
+        maxLength: "이미지는 최대 3장까지 선택할 수 있어요",
+        permissionRequest: "이미지를 추가하려면 사진 접근 권한이 필요해요",
+        addImageError: "사진 저장에 실패하였습니다",
+        choice: {
+          maxLength: "선택지는 최대 5개까지만 가능해요",
+          minLength: "선택지는 최소 2개가 있어야 해요",
+        },
+      },
+    },
+    startModal: {
+      headerLabel: "풀이 모드 선택하기",
+      startButtonLabel: "시작하기",
+      cancelButtonLabel: "취소",
+    },
   },
 };
